a9f6cd72c477076f281c3374b62dfeb23a8bcfaa
Gaxalco <gatien.penchaud@gmail.com>
2024-11-10 15:49:45 +0100

    Tested clang-format config (and found a good one)

--- assets/lib/SDL.c
+++ assets/lib/SDL.c
@@ -2,9 +2,6 @@
 
 /* Basic SDL functions */
 
-
-
-
 void start_SDL() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
@@ -14,11 +11,16 @@ void start_SDL() {
 
 void init(App app) {
     start_SDL();
-    app.window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    app.window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
     if (app.window == NULL) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
         exit(ERROR);
     }
+
     app.surface = SDL_GetWindowSurface(app.window);
 }
